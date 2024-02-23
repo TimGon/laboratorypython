73bd59ffd7cf833aa4492f7721dcf2fa3ccd651a
Timur <68633358+TimGon@users.noreply.github.com>
2024-02-23 16:56:03 +0400

    Update 2 lab1.py

--- lab1.py
+++ lab1.py
@@ -8,44 +8,60 @@
 Выводит на экран цифры числа, исключая семерки.
 Вычисляется среднее число между минимальным и максимальным и выводится прописью.
 """
+digit_to_word = {'0': 'ноль', '1': 'один', '2': 'два', '3': 'три', '4': 'четыре', '5': 'пять', '6': 'шесть',
+                 '7': 'семь', '8': 'восемь', '9': 'девять'}
 
-num_files = []
 num_filtered = []
-num_remove_seven = []
 max_num = 0
 min_num = 0
 
-with open("input.txt", "r") as file:
+
+def is_number(value):
+    symb_punct = '''!()[]{};:'"\\,<>./?@#$%^&*_~'''
+    fil_num = ''
+    for num_str in value:
+        if num_str not in symb_punct and not num_str.isalpha():
+            fil_num += num_str
+
+    return fil_num
+
+
+with open("input.txt", "r", encoding='utf-8') as file:
     for line in file:
-        string_files = line.strip()
-        numbers = string_files.split()
 
-        if numbers != '':
-            for num_str in numbers:
-                num = int(num_str)
-                num_files.append(num)
-            print("Список входных чисел из файла:", num_files)
-            for num in num_files:
-                if num % 2 != 0 and num < 4096 and str(num)[1] == '7' in str(num):
+        characters = line.split()
+
+        for char in characters:
+            num_fil = is_number(char)
+            is_negative = False
+
+            if num_fil.startswith('-'):
+                is_negative = True
+                num_fil = num_fil[1:]
+
+            if len(num_fil) > 1 and all('0' <= char <= '7' for char in num_fil):
+                num = int(num_fil)
+                if num % 2 != 0 and num < 4096 and str(num)[-2] == '7':
+                    if is_negative:
+                        num = -num
                     num_filtered.append(num)
-            print("Список удовлетворяющих условию числа:", num_filtered)
+if num_filtered:
+    print("Список чисел, удовлетворяющих условию:")
+    print(num_filtered)
 
-            if num_filtered:
+    if num_filtered:
+        min_num = min(num_filtered)
+        max_num = max(num_filtered)
 
-                min_num = min(num_filtered)
-                max_num = max(num_filtered)
+        print("Цифры чисел, исключая семерки:")
+        for num in num_filtered:
+            num_remove_seven = ''.join([digit for digit in str(abs(num)) if digit != '7'])
+            print(num_remove_seven)
 
-                print("Цифры чисел, исключая семерки:")
-                for num in num_filtered:
-                    num_remove_seven = ' '.join([digit for digit in str(num) if digit != '7'])
-                    print(num_remove_seven)
-
-                avg_num = (min_num + max_num) // 2
-                print(f"Среднее число между минимальным ({min_num}) и максимальным ({max_num}): {avg_num}")
-            else:
-                print("Нет подходящих чисел в последовательности.")
-
-            num_files = []
-            num_filtered = []
-        else:
-            print("\nФайл пустой. Добавьте непустой файл в директорию или переименуйте существующий *.txt файл")
+        avg_num = (min_num + max_num) // 2
+        avg_num_words = ' '.join([digit_to_word[digit] for digit in str(abs(avg_num))])
+        print(f"Среднее число между минимальным ({min_num}) и максимальным ({max_num}): {avg_num_words}")
+    else:
+        print("Нет чисел, удовлетворяющих условию.")
+else:
+    print("Нет чисел для обработки.")
