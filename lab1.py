a4f2f9a8e76cfa564da083fbc5f55b276c3e3793
Timur <68633358+TimGon@users.noreply.github.com>
2024-02-24 12:25:11 +0400

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -10,44 +10,32 @@
 """
 digit_to_word = {'0': 'ноль', '1': 'один', '2': 'два', '3': 'три', '4': 'четыре', '5': 'пять', '6': 'шесть',
                  '7': 'семь', '8': 'восемь', '9': 'девять'}
-
-num_filtered = []
-max_num = 0
 min_num = 0
+max_num = 10000
+num_filtered = []
+num_remove_seven = []
+file = open("input.txt", "r", encoding="utf-8")
 
-
-def is_number(value):
-    symb_punct = '''!()[]{};:'"\\,<>./?@#$%^&*_~'''
-    fil_num = ''
-    for num_str in value:
-        if num_str not in symb_punct and not num_str.isalpha():
-            fil_num += num_str
-
-    return fil_num
-
-
-with open("input.txt", "r", encoding='utf-8') as file:
-    for line in file:
-
-        characters = line.split()
-
-        for char in characters:
-            num_fil = is_number(char)
-            is_negative = False
-
-            if num_fil.startswith('-'):
-                is_negative = True
-                num_fil = num_fil[1:]
-
-            if len(num_fil) > 1 and all('0' <= char <= '7' for char in num_fil):
-                num = int(num_fil)
-                if num % 2 != 0 and num < 4096 and str(num)[-2] == '7':
-                    if is_negative:
-                        num = -num
-                    num_filtered.append(num)
+while True:
+    str_txt = file.readline().split()
+    print(str_txt)
+    if not str_txt:
+        print("\nФайл input.txt в директории проекта закончился")
+        break
+    else:
+        for num in str_txt:
+            flag_negative = False
+            symb_punct = '''!()[]{};:'"\\,<>./?@#$%^&*_~'''
+            if num.startswith('-'):
+                flag_negative = True
+                num = num[1:]
+            if all(val not in symb_punct and not val.isalpha() and '0' <= val <= '7' for val in num):
+                if int(num) % 2 != 0 and len(num) < 5 and num[-2] == '7':
+                    if flag_negative:
+                        num = -int(num)
+                    num_filtered.append(int(num))
 if num_filtered:
-    print("Список чисел, удовлетворяющих условию:")
-    print(num_filtered)
+    print("Список чисел, удовлетворяющих условию:\n",num_filtered)
     min_num = min(num_filtered)
     max_num = max(num_filtered)
 
@@ -61,4 +49,4 @@ if num_filtered:
     print(f"Среднее число между минимальным ({min_num}) и максимальным ({max_num}): {avg_num_words}")
 
 else:
-    print("Нет чисел для обработки/файл был пустым.")
+    print("Нет чисел для обработки.")
